fcb441f22140eb20a11cd1d4db6c4bdf2132bb61
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2022-12-10 10:34:33 +0800

    Add comment and format the file.

--- oxylabs/querySelectorAll.js
+++ oxylabs/querySelectorAll.js
@@ -1,15 +1,30 @@
 const puppeteer = require("puppeteer");
- 
+
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://en.wikipedia.org/wiki/Web_scraping");
- 
-  headings = await page.evaluate(() => {
-    headings_elements = document.querySelectorAll("h2 .mw-headline");
-    headings_array = Array.from(headings_elements);
-    return headings_array.map(heading => heading.textContent);
-  });
-  console.log(headings);
-  await browser.close();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://en.wikipedia.org/wiki/Web_scraping");
+
+    headings = await page.evaluate(() => {
+        headings_elements = document.querySelectorAll("h2 .mw-headline");
+        headings_array = Array.from(headings_elements);
+        return headings_array.map(heading => heading.textContent);
+    });
+    console.log(headings);
+    await browser.close();
 })();
+
+/**
+  Bonus tip: Array.from() function can be supplied with a map function directly, 
+  without a separate call to map. Depending on the comfort level, 
+  the same code can thus be written as:
+ */
+
+/**
+ * 
+  headings = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll("h2 .mw-headline"),
+      heading => heading.innerText.trim());
+  });
+
+ */
--- oxylabs/webScraping.js
+++ oxylabs/webScraping.js
@@ -1,13 +1,13 @@
 const puppeteer = require("puppeteer");
- 
+
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://en.wikipedia.org/wiki/Web_scraping");
-  
-  title = await page.evaluate(() => {
-    return document.querySelector("#firstHeading").textContent.trim();
-  });
-  console.log(title);
-  await browser.close();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://en.wikipedia.org/wiki/Web_scraping");
+
+    title = await page.evaluate(() => {
+        return document.querySelector("#firstHeading").textContent.trim();
+    });
+    console.log(title);
+    await browser.close();
 })();
